8980683cfb163d20c81293b01ba918ba993ee8e1
jth445600 <1360734170@qq.com>
2021-05-14 01:26:02 +0800

    Update cf-ip.sh

--- cf-ip.sh
+++ cf-ip.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 ##查找适合自己当前网络环境的优选Cloudflare Anycast IP
 #github项目地址: https://github.com/badafans/better-cloudflare-ip
-# wget -N --no-check-certificate -q -O install.sh "https://cdn.jsdelivr.net/gh/jth445600/hello-world@master/shiyan5.sh" && chmod +x install.sh && bash install.sh
-#
+# wget -N --no-check-certificate -q -O install.sh "https://cdn.jsdelivr.net/gh/jth445600/hello-world@master/cf-ip.sh" && chmod +x install.sh && bash install.sh
+#https://raw.githubusercontent.com/jth445600/hello-world/master/cf-ip.sh
 curl https://proxy.freecdn.workers.dev/?url=https://github.com/badafans/better-cloudflare-ip/releases/latest/download/linux.tar.gz -o linux.tar.gz
 tar -zvxf linux.tar.gz
 cd linux
