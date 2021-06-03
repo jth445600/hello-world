4bd34d380740c6f9d321d94567ed0ae3f5006c5b
jth445600 <1360734170@qq.com>
2021-06-04 00:18:50 +0800

    up

---
+++ termux_proxypoocheck.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd ~
+wget https://proxy.freecdn.workers.dev/?url=https%3A%2F%2Fgithub.com%2FSansui233%2FproxypoolCheck%2Freleases%2Fdownload%2Fv0.3.0%2FproxypoolCheck-linux-armv8-v0.3.0.gz -O proxypoolCheck-linux-armv8-v0.3.0.gz
