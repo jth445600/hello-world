083f698004644ff13f16a8759ca383eb7ce317fe
jth445600 <1360734170@qq.com>
2021-06-04 04:07:06 +0800

    Update termux_proxypoocheck.sh

--- termux_proxypoocheck.sh
+++ termux_proxypoocheck.sh
@@ -3,7 +3,7 @@
 # wget https://raw.githubusercontent.com/jth445600/hello-world/master/termux_proxypoocheck.sh && sh termux_proxypoocheck.sh
 ##
 cd ~
-pkg 
+
 wget https://cdn.jsdelivr.net/gh/jth445600/hello-world@master/proxypoolCheck
 
 chmod 775 proxypoolCheck
