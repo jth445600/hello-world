f8628b9b4db5c5e1ac93c098599c637e7f86120d
jth445600 <1360734170@qq.com>
2021-06-04 15:48:07 +0800

    Update termux_proxypoocheck.sh

--- termux_proxypoocheck.sh
+++ termux_proxypoocheck.sh
@@ -3,6 +3,13 @@
 # wget https://raw.githubusercontent.com/jth445600/hello-world/master/termux_proxypoocheck.sh && sh termux_proxypoocheck.sh
 ##
 cd ~
+# sed -i 's@^\(deb.*stable main\)$@#\1\ndeb https://mirrors.tuna.tsinghua.edu.cn/termux/termux-packages-24 stable main@' $PREFIX/etc/apt/sources.list
+
+# sed -i 's@^\(deb.*games stable\)$@#\1\ndeb https://mirrors.tuna.tsinghua.edu.cn/termux/game-packages-24 games stable@' $PREFIX/etc/apt/sources.list.d/game.list
+
+# sed -i 's@^\(deb.*science stable\)$@#\1\ndeb https://mirrors.tuna.tsinghua.edu.cn/termux/science-packages-24 science stable@' $PREFIX/etc/apt/sources.list.d/science.list
+
+# pkg update
 
 wget https://cdn.jsdelivr.net/gh/jth445600/hello-world@master/proxypoolCheck
 
